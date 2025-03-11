ed0035d232b99175b42690adeb49ce22ffb567d1
Rajan Dhinoja <rajandhinoja03@gmail.com>
2025-03-12 03:50:31 +0530

    up

--- app.py
+++ app.py
@@ -7,18 +7,18 @@ models = {
     "Breast Cancer": joblib.load("./saved_models/BREAST_CANCER_Prediction_Model.pkl"),
     "Diabetes": joblib.load("./saved_models/DIABETES_Disease_Prediction_Model.pkl"),
     "Heart Disease": joblib.load("./saved_models/HEART_Disease_Prediction_Model.pkl"),
-    "Liver Disease": joblib.load("./saved_models/LIVER_Disease_Prediction_Model.pkl"),
     "Kidney Disease": joblib.load("./saved_models/KIDNEY_Disease_Prediction_Model.pkl"),
+    "Liver Disease": joblib.load("./saved_models/LIVER_Disease_Prediction_Model.pkl"),
     "Parkinson's Disease": joblib.load("./saved_models/PARKINSON_Disease_Prediction_Model.pkl"),
 }
 
 # Input fields for different diseases
 disease_features = {
-    "Heart Disease": ["Age", "Sex", "BP", "Cholesterol"],
     "Breast Cancer": ["Radius Mean", "Texture Mean", "Perimeter Mean"],
-    "Liver Disease": ["Total Bilirubin", "Alkaline Phosphotase", "SGPT"],
-    "Kidney Disease": ["Blood Urea", "Serum Creatinine", "Hemoglobin"],
     "Diabetes": ["Glucose", "Blood Pressure", "BMI"],
+    "Heart Disease": ["Age", "Sex", "BP", "Cholesterol"],
+    "Kidney Disease": ["Blood Urea", "Serum Creatinine", "Hemoglobin"],
+    "Liver Disease": ["Total Bilirubin", "Alkaline Phosphotase", "SGPT"],
     "Parkinson's Disease": ["MDVP: Fo(Hz)", "MDVP: Fhi(Hz)", "MDVP: Flo(Hz)"],
 }
 
